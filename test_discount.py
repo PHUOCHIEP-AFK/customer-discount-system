eaac5ab408466416a69785ebc28a1960cc442b5b
Tran Phuoc Hiep <tranphuochiep16012005@gmail.com>
2026-06-08 20:26:19 +0700

    Implement loyalty customer discount feature

--- test_discount.py
+++ test_discount.py
@@ -4,7 +4,7 @@ def test_tc01():
     assert calculate_discount(60000000, 2000000) == 0.1
 
 def test_tc02():
-    assert calculate_discount(30000000, 2000000) == 0
+    assert calculate_discount(30000000, 2000000) == 0.0
 
 def test_tc03():
-    assert calculate_discount(49000000, 2000000) == 0
+    assert calculate_discount(49000000, 2000000) == 0.0
